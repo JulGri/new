649712cb5216b9f4ef0c5e0e5f9bfcb22c576ac9
Julia Grigorieva <grigorieva.js@gmail.com>
2015-11-28 15:51:39 +0300

    module KPI was created

--- local/modules/miet.kpi/lib/kpimanager.php
+++ local/modules/miet.kpi/lib/kpimanager.php
@@ -1,5 +1,5 @@
 <?php
-namespace MIET\READINGS;
+namespace MIET\KPI;
 use Bitrix\Main\Application;
 use Bitrix\Main\Entity;
 use Bitrix\Main\Entity\Event;
@@ -7,7 +7,7 @@ use Bitrix\Main\Localization\Loc;
 use Bitrix\Iblock\ElementTable;
 use Bitrix\Main\UserTable;
 Loc::loadMessages(__FILE__);
-class ReadingsManager {
+class KPIManager {
     const IBLOCK_CODE_KPI = 'kpi';
     const IBLOCK_CODE_DEPARTMENTS = 'departments';
     public static function GetKPI(
@@ -27,23 +27,23 @@ class ReadingsManager {
         $arIblock = $rsIblock->GetNext();
         $arFilter['IBLOCK_ID'] = $arIblock['ID'];
         $rsElements = \CIBlockElement::GetList(
-            $arOrder, //массив полей сортировки элементов и её направления
+            $arOrder, //массив полей сортировки элементов и еёнаправления
  $arFilter, //массив полей фильтра элементов и их значений
  $arGroupBy, //массив полей для группировки элементов
  $arNavStartParams, //параметры для постраничной навигациии ограничения количества выводимых элементов
  $arSelectFields //массив возвращаемых полей элементов
  );
  while($arElements = $rsElements->Fetch()) {
-     //Получение информации о файле с регламентом расчета показателя: ссылка на файл на сервере, название файла и т.д.
+     //Получение информации о файле с регламентом расчетапоказателя: ссылка на файл на сервере, название файла и т.д.
  foreach($arElements['PROPERTY_REGULATIONS_VALUE'] as $key
 => $idFileRegulation) {
          $arElements['PROPERTY_REGULATIONS_VALUE'][$key] =
              \CFile::GetFileArray($idFileRegulation);
      }
  $elements[] = $arElements;
-     }
- return $elements;
  }
+        return $elements;
+    }
     public static function GetKPIEmployee($idEmployee) {
         if(!$idEmployee) {
             return array();
@@ -101,4 +101,24 @@ class ReadingsManager {
             return true;
         }
     }
+    public static function GetKPIEmployeeValue($idKPI, $idEmployee, $period)
+    {
+        if (!$idKPI || !$idEmployee || !$period) {
+            return array();
+        }
+        return current(KPIEmployeeTablee::getList(array(
+            'select' => array(
+                'ID', 'UF_VALUE'
+
+            ),
+            'filter' => array(
+                'UF_PERIOD' => \Bitrix\Main\Type\Date\Time::createFromUserTime($period),
+                'UF_EMPLOYEE' => $idEmployee,
+                'UF_KPI' => $idKPI
+            )
+        ))->fetch()
+        );
+    }
 }
+
+
